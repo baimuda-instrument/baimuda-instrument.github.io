65cb6b8865b9e95ef34ce9bf71682d81d93fa996
steve <steve3816811205@gmail.com>
2021-08-20 16:35:54 +0800

    提出header部分css  增加mobile情況下選單

--- js/component/header.component.js
+++ js/component/header.component.js
@@ -1,3 +1,12 @@
-angular.module('MyApp').component('headerComp',{
-    templateUrl:'js/component/header.component.html'
+angular.module('MyApp').component('headerComp', {
+    templateUrl: 'js/component/header.component.html',
+    controllerAs: 'vm',
+    controller: function() {
+        var vm = this;
+        vm.openDropdown = false;
+
+        vm.toggleDropdown = function() {
+            vm.openDropdown = !vm.openDropdown;
+        }
+    }
 })
