bcd0478e277a3ec9c8a6f7e6075b8d9ab1ca0da0
steve <steve3816811205@gmail.com>
2021-08-23 16:34:53 +0800

    商品列表項目增加英文

--- js/service/productService.service.js
+++ js/service/productService.service.js
@@ -1,22 +1,27 @@
 angular.module('MyApp').service('ProductService', function() {
     this.productData = [{
             name: '攪拌機',
+            engName: 'stirrer',
             url: "/#!product/stirrer"
         },
         {
             name: '攪拌機配件&翼片',
+            engName: 'stirrer accessory',
             url: '/#!product/stirrer-accessory'
         },
         {
             name: '電氣加熱板',
+            engName: 'heating plate',
             url: '/#!product/heating-plate'
         },
         {
             name: '離心機',
+            engName: 'centrifuge',
             url: '/#!product/centrifuge'
         },
         {
             name: '真空幫浦',
+            engName: 'vacummn pump',
             url: '/#!/product/vacummn-pump'
         }
 
