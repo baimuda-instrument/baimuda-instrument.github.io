6db6780d90fd28aa666f55b98ae164613964b271
steve <steve3816811205@gmail.com>
2021-08-23 20:54:12 +0800

    設定不知道state時導到iindex狀態

--- js/route/route.config.js
+++ js/route/route.config.js
@@ -1,28 +1,35 @@
 angular.module('MyApp').config(
-    
-    function($stateProvider,$urlRouterProvider){
 
-    $stateProvider.state({
-        name: 'index',
-        url: '',
-        views:{
-            'main@':{
-                component:'indexComp'
+    function($stateProvider, $urlRouterProvider) {
+
+        $stateProvider.state({
+            name: 'index',
+            url: '',
+            views: {
+                'main@': {
+                    component: 'indexComp'
+                }
             }
-        }
+        })
+
+        $stateProvider.state({
+            name: 'contact',
+            url: '/contact',
+            views: {
+                'main@': {
+                    component: 'contactComp'
+                }
+            }
+        });
+
+        // $urlRouterProvider.otherwise('/');
+        $urlRouterProvider.otherwise(function($injector, $location) {
+            var $state = $injector.get('$state');
+            if ($location.$$url === '' || $location.$$url === '/') {
+                $state.go('index');
+                return;
+            }
+            $state.go('index'); // some custom not found page
+        });
+
     })
-
-    $stateProvider.state({
-        name:'contact',
-        url:'/contact',
-        views:{
-            'main@':{
-                component: 'contactComp'
-            }
-        }
-    });
-
-    $urlRouterProvider.otherwise('/');
-
-    
-})
