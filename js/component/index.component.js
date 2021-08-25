d53009aafa5d250854f949ec1ac0ee7c2e7c35c2
steve <steve3816811205@gmail.com>
2021-08-25 14:24:31 +0800

    首頁增加圖片和開卷軸特效

--- js/component/index.component.js
+++ js/component/index.component.js
@@ -1,3 +1,10 @@
-angular.module('MyApp').component('indexComp',{
-    templateUrl:'js/component/index.component.html'
+angular.module('MyApp').component('indexComp', {
+    templateUrl: 'js/component/index.component.html',
+    controller: function() {
+
+        var vm = this;
+        vm.$onInit = function() {
+            angular.element('.main .index .scroll-effect-cover').css('width', '0%');
+        }
+    }
 })
