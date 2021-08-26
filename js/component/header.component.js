c32e9da015c10fbd80bf254b81bcfdfba1ee43b7
steve <steve3816811205@gmail.com>
2021-08-26 11:51:10 +0800

    mobile navbar改成整全屏面板

--- js/component/header.component.js
+++ js/component/header.component.js
@@ -3,10 +3,12 @@ angular.module('MyApp').component('headerComp', {
     controllerAs: 'vm',
     controller: function() {
         var vm = this;
+        var myCollapseDiv = $('.myCollapse');
         vm.openDropdown = false;
 
         vm.toggleDropdown = function() {
             vm.openDropdown = !vm.openDropdown;
+            myCollapseDiv.css('right', vm.openDropdown ? '0' : '-100%');
         }
     }
 })
