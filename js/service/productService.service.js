b83beb55a12eed18a869605380f108e63588eaaa
steve <steve3816811205@gmail.com>
2021-09-01 17:29:27 +0800

    增加一些產品項目  還沒做內頁

--- js/service/productService.service.js
+++ js/service/productService.service.js
@@ -15,20 +15,56 @@ angular.module('MyApp').service('ProductService', function() {
             url: '/#!product/heating-plate'
         },
         {
+            name: '藥錠試驗器系列',
+            engName: 'tablet tester',
+            url: '/#!/product/tablet-tester'
+        },
+        {
+            name: '振盪 & 萃取& 混合器系列',
+            engName: '',
+            url: ''
+        },
+        {
+            name: '電磁加熱攪拌器',
+            engName: '',
+            url: ''
+        },
+        {
+            name: '乳化&均質機',
+            engName: '',
+            url: ''
+        },
+        {
             name: '離心機',
             engName: 'centrifuge',
             url: '/#!product/centrifuge'
+        },
+        {
+            name: '標準篩震盪器',
+            engName: '',
+            url: ''
+        },
+        {
+            name: '球磨機',
+            engName: 'Ball Mill',
+            url: ''
+        },
+        {
+            name: 'COD分解 / 乾浴',
+            engName: '',
+            url: ''
+        },
+        {
+            name: '恆溫 水(油) 浴鍋槽',
+            engName: '',
+            url: ''
         },
         {
             name: '真空幫浦',
             engName: 'vacummn pump',
             url: '/#!/product/vacummn-pump'
         },
-        {
-            name: '藥錠試驗器系列',
-            engName: 'tablet tester',
-            url: '/#!/product/tablet-tester'
-        }
+
 
     ];
 })
