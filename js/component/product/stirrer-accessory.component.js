dc12999c2fa4a336346267538a800b00fd7973f4
steve <steve3816811205@gmail.com>
2021-09-02 17:59:02 +0800

    增加攪拌機葉片圖片

--- js/component/product/stirrer-accessory.component.js
+++ js/component/product/stirrer-accessory.component.js
@@ -1,3 +1,8 @@
 angular.module('MyApp').component('stirrerAccessory', {
-    template: '<div><img src="img/product/攪拌機配件.jpg"></div>'
+    template: `<div>
+                    <img src="img/product/攪拌機配件.jpg">
+                    <img src="img/product/攪拌機配件2.jpg">
+                    <img src="img/product/攪拌機配件3.jpg">
+                    <img src="img/product/攪拌機配件4.jpg">
+                </div>`
 });
