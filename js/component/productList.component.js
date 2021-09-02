8f85cda9d9544b834c0192c2db7a98d892edec66
steve <steve3816811205@gmail.com>
2021-09-02 16:29:08 +0800

    mobile時增加productList收合功能

--- js/component/productList.component.js
+++ js/component/productList.component.js
@@ -1,9 +1,15 @@
-angular.module('MyApp').component('productList',{
-templateUrl:'js/component/productList.component.html',
-controllerAs:'vm',
-controller:function(ProductService){
-var vm = this;
-vm.productData = ProductService.productData;
-}
+angular.module('MyApp').component('productList', {
+    templateUrl: 'js/component/productList.component.html',
+    controllerAs: 'vm',
+    controller: function(ProductService) {
+        var vm = this;
+        vm.productData = ProductService.productData;
+
+        vm.collapseList = false;
+
+        vm.toggle = function() {
+            vm.collapseList = !vm.collapseList;
+        }
+    }
 
 });
