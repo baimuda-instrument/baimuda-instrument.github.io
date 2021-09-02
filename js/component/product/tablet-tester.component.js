06e4f1e6bf549d30aeec94b84ed9283f209d74f0
steve <steve3816811205@gmail.com>
2021-09-02 11:02:52 +0800

    增加凝集試驗器頁下載按鈕及樣式

--- js/component/product/tablet-tester.component.js
+++ js/component/product/tablet-tester.component.js
@@ -19,5 +19,7 @@ angular.module('MyApp')
         template: '<div><img src="img/product/錠劑硬度計.jpg" /></div>'
     })
     .component('jt', { // 凝集試驗器 JT-6S ??
-        template: '<div><img src="img/product/BMD-凝集試驗器.jpg" /></div>'
+        template: `<div><img src="img/product/BMD-凝集試驗器.jpg" /></div>
+        <div class="download-container"><a class="download-btn" href="img/product/BMD-凝集試驗器.jpg" download>下載目錄</a></div>
+        `
     })
